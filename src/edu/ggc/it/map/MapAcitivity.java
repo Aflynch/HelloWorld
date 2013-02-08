a6b984787314847fa8fbddd45b7d67786abd365e
JohnnyLynch <alynch@ggc.edu>
2013-02-08 13:32:18 -0500

    A new Activity Called MapActivity was add. Its view only has text.

--- src/edu/ggc/it/map/MapAcitivity.java
+++ src/edu/ggc/it/map/MapAcitivity.java
@@ -1,5 +1,14 @@
 package edu.ggc.it.map;
 
-public class MapAcitivity {
+import android.app.Activity;
+import android.os.Bundle;
+import edu.ggc.it.R;
+
+public class MapAcitivity extends Activity {
+	
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.maplayout);
+	}
 
 }
